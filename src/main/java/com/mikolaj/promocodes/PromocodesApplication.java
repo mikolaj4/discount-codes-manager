da9f7f6a98fd72eb2ea10ff737cf15d0f533737b
mikolaj <mikolaj.gawor@protonmail.com>
2024-05-08 13:45:25 +0200

    initial commit

---
+++ src/main/java/com/mikolaj/promocodes/PromocodesApplication.java
@@ -0,0 +1,13 @@
+package com.mikolaj.promocodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikolaj/promocodes/PromocodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikolaj.promocodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
