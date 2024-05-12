5db032899bc75a7d06a44b72a74fa9d32259f05a
mikolaj <mikolaj.gawor@protonmail.com>
2024-05-12 19:48:10 +0200

    Add validation to dtos

--- src/main/java/com/mikolaj/promocodes/application/dtos/discount_dtos/DiscountPriceDto.java
+++ src/main/java/com/mikolaj/promocodes/application/dtos/discount_dtos/DiscountPriceDto.java
@@ -1,8 +1,7 @@
 package com.mikolaj.promocodes.application.dtos.discount_dtos;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
@@ -10,8 +9,11 @@ public class DiscountPriceDto {
     @Valid
 
     @NotEmpty(message = "code name is mandatory")
+    @Size(max = 24, message = "length must be <= 24")
     private String codeName;
 
     @NotNull(message = "product id is mandatory")
+    @Min(value = 1, message = "product indexing starts at 1")
+    @Max(value = 9999999, message = "there wont more than 10 million products in the database, cmon...")
     private Integer productId;
 }
--- src/main/java/com/mikolaj/promocodes/application/dtos/purchase_dtos/CreatePurchaseDto.java
+++ src/main/java/com/mikolaj/promocodes/application/dtos/purchase_dtos/CreatePurchaseDto.java
@@ -1,7 +1,10 @@
 package com.mikolaj.promocodes.application.dtos.purchase_dtos;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,7 +12,10 @@ public class CreatePurchaseDto {
     @Valid
 
     @NotNull(message = "product id is mandatory")
+    @Min(value = 1, message = "product indexing starts at 1")
+    @Max(value = 9999999, message = "there wont more than 10 million products in the database, cmon...")
     private Integer productId;
 
+    @Size(max = 24, message = "length must be <= 24")
     private String codeName;
 }
